25dd2fd7b98e6e7402bdb28473fb1ab34fe7f5ac
Erik Diers <erik.diers@ruhr-uni-bochum.de>
2026-07-11 23:28:19 +0200

    Explanation

--- FrontEnd/BadmintonFrontend/src/components/SubscribeCalenderButton.vue
+++ FrontEnd/BadmintonFrontend/src/components/SubscribeCalenderButton.vue
@@ -1,8 +1,9 @@
 <template>
-    <div>
+    <div class="p-2">
         <p>Kopiere die URL <a :href="webcalURL">{{webcalURL}}</a> als externen Kalender in die Kalender-App deiner Wahl.</p><br>
         <p>Für Google drücke den untenliegenden Knopf und füge die URL in das "URL für Kalender" Feld. Drücke anschließend "Kalender hinzufügen".</p>
         <button @click="copyURL()" class="btn">Subscribe for Google Calender</button>
+        <p class="text-xs">Falls der Kalender nicht auf der Handy-App angezeigt wird: Gehe in Kalender Settings (Drei Strich Menu -> Settings). Under den Kalendern sollte nun der https://tg.dondevelops... Kalender auftauchen (evntl. unter "Mehr anzeigen"). Darauf klicken und dann Synchronisierne Einschalte. In den Settings kann nun der Kalender per Checkbox aktiviert werden.</p>
     </div>
 </template>
 
